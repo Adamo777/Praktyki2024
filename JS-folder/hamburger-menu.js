da11326d747d2da30c2bb9abf9a13e4ff2855b3f
Adam Juszczyk <juszczykadam71@gmail.com>
2024-10-10 18:52:03 +0200

    BEM
    wszystko dziala. do zrobienia jeszcze BEM na klasy <section>

--- JS-folder/hamburger-menu.js
+++ JS-folder/hamburger-menu.js
@@ -1,6 +1,6 @@
 const toggleMenu = () => {
-  const menuBtn = document.querySelector(".menu-swipe");
-  const navUlListUnvisible = document.querySelector(".nav-ul-list-unvisible")
+  const menuBtn = document.querySelector(".nav__menu--swipe");
+  const navUlListUnvisible = document.querySelector(".nav__list-unvisible")
   menuBtn.classList.toggle("visible");
   navUlListUnvisible.classList.toggle("unvisible")
 };
