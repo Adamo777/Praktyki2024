569e33d7dc5e02c6468670a88a9437f6eb725177
Adamo777 <juszczykadam71@gmail.com>
2024-10-11 10:46:37 +0200

    fixed
    Na wszystkim BEM
    przed rwd section

--- JS-folder/hamburger-menu.js
+++ JS-folder/hamburger-menu.js
@@ -1,8 +1,9 @@
 
 const toggleMenu = () => {
   const menuBtn = document.querySelector(".nav__menu--swipe");
-  const navUlListUnvisible = document.querySelector(".nav__list-unvisible")
+  const navUlListUnvisible = document.querySelector(".nav__list--ul-unvisible")
   menuBtn.classList.toggle("visible");
   navUlListUnvisible.classList.toggle("unvisible")
+
 
 };
