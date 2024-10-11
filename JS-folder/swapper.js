5b26abc2f0021a2b6cf30b5e71abcb46fd140ae1
Adamo777 <juszczykadam71@gmail.com>
2024-10-11 08:28:40 +0200

    BEM i zagnieżdżanie

--- JS-folder/swapper.js
+++ JS-folder/swapper.js
@@ -1,7 +1,6 @@
 const swapper = () => {
   const firstSlide = document.querySelector(".section__first-slide");
   const secondSlide = document.querySelector(".section__second-slide");
-  console.log("sigma");
 
   firstSlide.classList.toggle("unvisible-slide");
   secondSlide.classList.toggle("unvisible-slide");
