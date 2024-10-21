40f944c8717eb731ceb0401e16672e8379254d23
Adamo777 <juszczykadam71@gmail.com>
2024-10-21 14:56:20 +0200

    commit
    do poprawy BEM

--- JS/swapper.js
+++ JS/swapper.js
@@ -1,17 +1,43 @@
-"use strict";
-
 (function () {
-    const firstSlide = document.querySelector(".section__first-slide");
-    const secondSlide = document.querySelector(".section__second-slide");
+  const slides = document.querySelectorAll(".mainSlider__slide"); // Zbieramy wszystkie slajdy
+  const leftBtn = document.querySelector(".mainSlider__btn-left");
+  const rightBtn = document.querySelector(".mainSlider__btn-right");
+  let currentIndex = 0;
+  let autoSlideInterval;
 
-    firstSlide.addEventListener("click", () =>{
-      firstSlide.classList.toggle("unvisible-slide");
-      secondSlide.classList.toggle("unvisible-slide");
-    })
+  const changeSlide = (direction) => {
+    slides[currentIndex].classList.add("unvisible-slide");
 
-    secondSlide.addEventListener("click", () => {
-      firstSlide.classList.toggle("unvisible-slide");
-      secondSlide.classList.toggle("unvisible-slide");
-    })
+    if (direction === "left") {
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    } else if (direction === "right") {
+      currentIndex = (currentIndex + 1) % slides.length;
+    }
 
+    slides[currentIndex].classList.remove("unvisible-slide");
+  };
+
+  const startAutoSlide = () => {
+    autoSlideInterval = setInterval(() => {
+      changeSlide("right");
+    }, 7000);
+  };
+
+  const stopAutoSlide = () => {
+    clearInterval(autoSlideInterval);
+  };
+
+  leftBtn.addEventListener("click", () => {
+    stopAutoSlide();
+    changeSlide("left");
+    startAutoSlide();
+  });
+
+  rightBtn.addEventListener("click", () => {
+    stopAutoSlide();
+    changeSlide("right");
+    startAutoSlide();
+  });
+
+  startAutoSlide();
 })();
