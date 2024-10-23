9b7566fef916fa3255836bb2a994cc8d00be7384
Adamo777 <juszczykadam71@gmail.com>
2024-10-23 16:04:34 +0200

    last

--- JS/responsiveFooter.js
+++ JS/responsiveFooter.js
@@ -1,12 +1,12 @@
 "use strict";
 
 (function () {
-    const textBtns = document.querySelectorAll(".section__info-item-text");
+    const textBtns = document.querySelectorAll(".footer__infoHeading");
   
     textBtns.forEach(button => {
       button.addEventListener('click', function () {
-        const parentItem = button.closest('.section__info-item');
-        const list = parentItem.querySelector('.section__info-list');
+        const parentItem = button.closest('.footer__info');
+        const list = parentItem.querySelector('.footer__list');
         list.classList.toggle('active');
       });
     });
